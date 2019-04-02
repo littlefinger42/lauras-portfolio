39bee313b05bc5cda7d6696fa58f5246cef8eb66
littlefinger42 <lauriemail@protonmail.com>
2019-04-02 17:18:25 +0200

    Revert "Added a hover affect on posts"
    This reverts commit 3f98e693db275b7e972f8d917cb6a602ee8b4718.

--- src/components/post.js
+++ src/components/post.js
@@ -5,59 +5,32 @@ import { device } from "./device"
 import { Link } from "gatsby"
 
 const PropItem = styled.div`
-  flex: 0 1 100%;
-  text-align: center;
-  article {
-    position: relative;
-  }
-  a {
-    z-index: 11;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-  }
-  > * {
-    margin: 0 ${device.padding.mobileS};
-  }
+	flex: 0 1 100%;
+	text-align: center;
+	
+	>* {
+		padding: 0 ${device.padding.mobileS};
+	}
+
   @media ${device.mediaQuery.tablet} {
     flex: 0 1 50%;
-    > * {
-      margin: 0 ${device.padding.tablet};
-    }
+		>* {
+			padding: 0 ${device.padding.tablet};
+		}
   }
+
   @media ${device.mediaQuery.laptop} {
     flex: 0 1 33%;
   }
 `
 
-const ImgContainer = styled.div`
-  &:after {
-    content: "";
-    z-index: 10;
-    display: block;
-    position: absolute;
-    height: 100%;
-    top: 0;
-    left: 0;
-    right: 0;
-    background: rgba(0, 0, 0, 0.5);
-    transition: all 0.3s;
-  }
-  &:hover:after {
-    background: rgba(0, 0, 0, 0);
-  }
-`
-
-export default (props, { children }) => (
+export default (props, { children} ) => (
   <PropItem>
     <article>
-      <ImgContainer>
-        <img src="https://source.unsplash.com/random" width="100%" />
-        <Link to={props.key}>
-          <button>{props.title}</button>
-        </Link>
-      </ImgContainer>
+      <img src="https://source.unsplash.com/random" width="100%"></img>
+      <Link to={props.key}>
+        <h1>{props.title}</h1>
+      </Link>
       {children}
       <small>{props.date}</small>
     </article>
