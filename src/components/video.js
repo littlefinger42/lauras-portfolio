2b1cd35ad222cad6d8fb8772c190e1e007eba04e
littlefinger42 <lauriemail@protonmail.com>
2019-04-18 13:18:22 +0200

    Made the showreel always 1080p

--- src/components/video.js
+++ src/components/video.js
@@ -2,8 +2,6 @@ import React from "react"
 import styled from "styled-components"
 
 import video1080 from "../../static/assets/Showreel1080.mp4"
-import video720 from "../../static/assets/Showreel720.mp4"
-import video480 from "../../static/assets/Showreel480.mp4"
 
 const StyledVideo = styled.video`
   width: 100%;
@@ -12,16 +10,6 @@ const StyledVideo = styled.video`
 
 export default () => (
   <StyledVideo async controls>
-    <source
-      src={video480}
-      type="video/mp4"
-      media="all and (max-width: 480px)"
-    />
-    <source
-      src={video720}
-      type="video/mp4"
-      media="all and (max-width: 720px)"
-    />
-    <source src={video1080} type="video/mp4" />
+    <source src={video1080} type="video/mp4"/>
   </StyledVideo>
 )
