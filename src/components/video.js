3cc13b0907e2b88714273fb74b7bc1010415f639
littlefinger42 <lauriemail@protonmail.com>
2019-04-23 16:05:05 +0200

    Added a video thumbnail

--- src/components/video.js
+++ src/components/video.js
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "styled-components"
 
 import video1080 from "../../static/assets/Showreel1080.mp4"
+import videoPoster from "../../static/assets/showreelthumbnail.jpg"
 
 const StyledVideo = styled.video`
   width: 100%;
@@ -9,7 +10,7 @@ const StyledVideo = styled.video`
 `
 
 export default () => (
-  <StyledVideo async controls>
+  <StyledVideo async controls poster={videoPoster}>
     <source src={video1080} type="video/mp4"/>
   </StyledVideo>
 )
